1b76a6c4e234b2682d992c27b2e8d5b13081d01e
Sudheesh.S.Babu <sudheesh_s@reizend.in>
2019-05-22 10:29:49 +0530

    Include Company.php mapping file before entity generation

--- src/AppBundle/Entity/Company.php
+++ src/AppBundle/Entity/Company.php
@@ -11,7 +11,7 @@ class Company {
 
 	/**
 	* @var integer $id
-	* @ORM\Column(type="int")
+	* @ORM\Column(type="integer")
 	* @ORM\Id
 	* @ORM\GeneratedValue(strategy="IDENTITY")
 	*/
@@ -34,89 +34,7 @@ class Company {
 	/**
 	* @var integer serial_no
 	*
-	* @ORM\Column(type="int")
+	* @ORM\Column(type="integer")
 	*/
 	private $serial_no;
-
-    /**
-     * Get id
-     *
-     * @return \int
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
-
-    /**
-     * Set name
-     *
-     * @param string $name
-     *
-     * @return Company
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    /**
-     * Get name
-     *
-     * @return string
-     */
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    /**
-     * Set address
-     *
-     * @param string $address
-     *
-     * @return Company
-     */
-    public function setAddress($address)
-    {
-        $this->address = $address;
-
-        return $this;
-    }
-
-    /**
-     * Get address
-     *
-     * @return string
-     */
-    public function getAddress()
-    {
-        return $this->address;
-    }
-
-    /**
-     * Set serialNo
-     *
-     * @param \int $serialNo
-     *
-     * @return Company
-     */
-    public function setSerialNo(\int $serialNo)
-    {
-        $this->serial_no = $serialNo;
-
-        return $this;
-    }
-
-    /**
-     * Get serialNo
-     *
-     * @return \int
-     */
-    public function getSerialNo()
-    {
-        return $this->serial_no;
-    }
 }
