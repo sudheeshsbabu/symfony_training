34cfdcae779fcfbcc2f579e9a14e950a58132fd1
Sudheesh.S.Babu <sudheesh_s@reizend.in>
2019-05-23 10:11:58 +0530

    Include EmployeeBundle & TestBundle with necessary changes
    Including manual registeration of bundles in composer.json under autoload section
    Including twig template engine to config.yml

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
+            new EmployeeBundle\EmployeeBundle(),
+            new TestBundle\TestBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
