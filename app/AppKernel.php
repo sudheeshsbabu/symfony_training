6c0034c27cbd7cff9328742ed9360650fc88393b
Sudheesh.S.Babu <sudheesh_s@reizend.in>
2019-05-30 12:08:48 +0530

    Include FOSUserBundle along with changes in security.yml, config.yml & parameters.yml.

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -19,6 +19,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new EmployeeBundle\EmployeeBundle(),
             new TestBundle\TestBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
