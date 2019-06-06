f62e6d778aca344be46be689fe4c9db0cef0d515
Sudheesh.S.Babu <sudheesh_s@reizend.in>
2019-06-06 12:53:55 +0530

    Fetch data from mongodb document.

--- src/ProductBundle/Controller/DefaultController.php
+++ src/ProductBundle/Controller/DefaultController.php
@@ -12,14 +12,24 @@ class DefaultController extends Controller
     public function indexAction()
     {
 //        return $this->render('ProductBundle:Default:index.html.twig');
-        $product = new Product();
-        $product->setName('A Foo Bar');
-        $product->setPrice('19.99');
+//        $product = new Product();
+//        $product->setName('A Foo Bar');
+//        $product->setPrice('19.99');
+//
+//        $dm = $this->get('doctrine_mongodb')->getManager();
+//        $dm->persist($product);
+//        $dm->flush();
+//
+//        return new Response('Created product id '.$product->getId());
+            $id = '5cf8bc2daa485b43d3703671';
+            $product = $this->get('doctrine_mongodb')
+                ->getRepository('ProductBundle:Product')
+                ->find($id);
 
-        $dm = $this->get('doctrine_mongodb')->getManager();
-        $dm->persist($product);
-        $dm->flush();
-
-        return new Response('Created product id '.$product->getId());
+            if (!$product) {
+                throw $this->createNotFoundException('No product found for id '.$id);
+            }
+            return new Response('Created product id '.$product->getId());
+        
     }
 }
